9bdb3903ed0e3886119d09e80c667403c31337b9
MarinaNikodim <marinenik@yandex.ru>
2024-08-19 19:24:00 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -8,8 +8,8 @@ my_dict_schedule['Have a shower']=23.15
 my_dict_schedule['Dinner']=19.30
 my_dict_schedule['Go to bed']=23.45
 print(my_dict_schedule)
-del my_dict_schedule['Go to bed']
-print(my_dict_schedule)
+a=my_dict_schedule.pop('Go to bed')
+print(a)
 my_dict_schedule.update({'Wake_up:': 07.00, 'Training:': 07.15, 'Breakfast:': 07.45})
 print(my_dict_schedule)
 #set
